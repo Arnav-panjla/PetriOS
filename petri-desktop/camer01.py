9bd27912a5fa5c52855b62031aeca18f70f0f706
Manashvi1205 <gargmanashvi@gmail.com>
2026-08-05 11:34:24 +0530

    Add petri-desktop folder

---
+++ petri-desktop/camer01.py
@@ -0,0 +1,13 @@
+from picamera2 import Picamera2
+import time
+
+picam2 = Picamera2()
+
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+
+picam2.start()
+time.sleep(2)          # Allow camera to adjust exposure
+picam2.capture_file("test.jpg")
+
+picam2.stop()
